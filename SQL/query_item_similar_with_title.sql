d50b71f6ca9ea6e665053fd86440ed7c6186761f
Sean Chuang <chunghsiang.chuang@smartnews.com>
2020-06-23 08:54:00 +0800

    [SQL] update sQL

--- SQL/query_item_similar_with_title.sql
+++ SQL/query_item_similar_with_title.sql
@@ -125,7 +125,7 @@ high_freq_items as (
     group by 1 order by 2 desc 
     limit 4000 
 ),
-with item_emb as (
+item_emb as (
     select 
         element_at(split(item, ':',2),1) as event,  
         element_at(split(item, ':',2),2) as content_id, 
@@ -139,8 +139,8 @@ item_emb1 as (
         content_id,
         MAX_BY(vec, 
             case 
-                when event='purchase' then 2 
-                when event='add_to_cart' then 1 
+                when event='revenue' then 2 
+                when event='AddToCart' then 1 
                 else 0 
             end) as vec
     from item_emb
@@ -151,27 +151,31 @@ item_emb2 as (
         content_id, 
         vec
     from item_emb
-    where event='view_content'
+    where event='ViewContent'
 ),
 item_item_similarity as (
     select 
         a1.content_id as item_a,
         a2.content_id as item_b,
+        cat.title as item_b_title,
+        cat.google_product_category as item_b_category,
         reduce(zip_with(a1.vec, a2.vec, (x,y)->x*y), 0, (s,x)->s+x, s->s) 
                         / pow(reduce(a1.vec, 0,(s,x)->s+x*x,s->s),0.5) 
                         / pow(reduce(a2.vec, 0,(s,x)->s+x*x,s->s), 0.5) as score
     from item_emb2 a1
     cross join item_emb2 a2
-    inner join high_freq_items h on a1.item_a = h.content_id 
-    inner join catlog_items cat on a2.item_b = cat.content_id
+    inner join high_freq_items h on a1.content_id = h.content_id 
+    inner join catlog_items cat on a2.content_id = cat.content_id
 )
-select count(distinct item_a) as c_item1 , count(distinct item_b) as c_item2 from item_item_similarity;
-
 select 
     item_a as item,
-    slice(array_agg(concat(item_b, '=', cast(score AS VARCHAR)) order by score desc), 1, 20) as similar_item,
-    '2020-05-30-w20_n2_ft' as dt
+    slice(array_agg((item_b, score, item_b_title, item_b_category) order by score desc), 1, 20) as similar_item,
+    '2020-05-30-w20_n2_ft' as tag
 from item_item_similarity
 group by 1
 ;
 
+
+select count(distinct item_a) as c_item1 , count(distinct item_b) as c_item2 from item_item_similarity;
+
+select * from item_item_similarity limit 100;
