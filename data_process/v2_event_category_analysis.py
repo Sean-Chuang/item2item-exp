3c9a0f84d0f97b4f1f72cf523d40c0a645fd062c
Sean Chuang <chunghsiang.chuang@smartnews.com>
2020-07-05 21:44:18 +0800

    [v2] update scripts

--- data_process/v2_event_category_analysis.py
+++ data_process/v2_event_category_analysis.py
@@ -41,25 +41,56 @@ def get_item_google_category(file_name='items_info.csv'):
     data = data.set_index('content_id')
     return data
 
-def get_user_activities_list(input_file, file_name='user_activies.csv', topK=500000):
+def get_user_events(input_file, activies_file_name='user_activies.csv', topK=500000):
     user_activities_count = defaultdict(int)
-    if not os.path.exists(file_name):
-        with open(file_name, 'r') as in_f:
+    if not os.path.exists(activies_file_name):
+        with open(input_file, 'r') as in_f:
             for line in tqdm(in_f):
                 ad_id, item_id, behavior, ts = line.strip().split('\t')
                 if item_id:
                     user_activities_count[ad_id] += 1
         data = pd.DataFrame(list(user_activities_count.items()), columns=['ad_id', 'count'])
         data = data.sort_values(by='count', ascending=False)
-        data.to_csv(file_name, sep='\t')
+        data.to_csv(activies_file_name, sep='\t')
     else:
-        data = pd.read_csv(file_name, sep='\t')
+        data = pd.read_csv(activies_file_name, sep='\t')
+    user_topK_list = data.head(topK)['ad_id'].tolist()
 
-    return data.head(topK)['ad_id'].tolist()
+    user_events = defaultdict(list)
+    with open(input_file, 'r') as in_f:
+        for line in tqdm(in_f):
+            ad_id, item_id, behavior, ts = line.strip().split('\t')
+            if item_id and ad_id in user_topK_list:
+                user_events[ad_id].append((item_id, behavior, int(ts)))
+    log.info(f"Total user : {len(user_events)}")
+    return user_events
 
 
-def get_user_sessions(data, topK_user):
-    pass
+def get_user_sessions(user_events, session_period=3600):
+    user_sessions = defaultdict(list)
+    if session_period:
+        for ad_id in tqdm(user_events):
+            tmp = []
+            user_events[ad_id].sort(key = lambda x:x[-1])
+            for event in user_events[ad_id]:
+                if len(tmp) == 0:
+                    tmp.append(event)
+                else:
+                    if event[-1] - tmp[-1][-1] < session_period:
+                        tmp.append(event)
+                    else:
+                        user_sessions[ad_id].append(tmp)
+                        tmp = [event]
+            if len(tmp) > 0:
+                user_sessions[ad_id].append(tmp)
+
+    else:
+        for ad_id in tqdm(user_events):
+            user_events[ad_id].sort(key = lambda x:x[-1])
+            user_sessions[ad_id].append(user_events[ad_id])
+
+    return user_sessions
+
 
 def category_relation_analyasis():
     pass
@@ -67,8 +98,8 @@ def category_relation_analyasis():
 
 def main():
     items_info = get_item_google_category()
-    user_topK = get_user_activities_list("/mnt1/train/item2item-exp/data/2020-05-30/tr_data/merged.data")
-
+    user_events = get_user_events("/mnt1/train/item2item-exp/data/2020-05-30/tr_data/merged.data")
+    user_sessions = get_user_sessions(user_events, session_period=3600)
 
 if __name__ == '__main__':
     main()
