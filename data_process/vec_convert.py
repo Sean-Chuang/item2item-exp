6f554070a59e2a08e096c64858c34ab75c73f475
Sean Chuang <chunghsiang.chuang@smartnews.com>
2020-07-12 09:04:21 +0800

    [data_process] refine vector

--- data_process/vec_convert.py
+++ data_process/vec_convert.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import os
 import sys
+from tqdm import tqdm
 
 vecfile = sys.argv[1]
 print(f'Vector file : {vecfile}')
+new_file = vecfile + '.upload'
 
-with open(vecfile, 'r') as in_f, open('upload_' + vecfile, 'w') as out_f:
+with open(vecfile, 'r') as in_f, open(new_file, 'w') as out_f:
     num_items, dim = in_f.readline().strip().split()
     print(f'Num of items : {num_items}, dim : {dim}')
     
@@ -16,4 +18,4 @@ with open(vecfile, 'r') as in_f, open('upload_' + vecfile, 'w') as out_f:
 
         print(f"{idx}\t{item_id}\t{','.join(emb)}", file=out_f)
 
-print(f'Save vector file : {'upload_' + vecfile}')
+print(f'Save vector file : {new_file}')
